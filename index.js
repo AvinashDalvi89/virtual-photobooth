015f49130fba30c0cc4b5556dc520695914f8473
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-11-14 00:05:01 +0530

    Added new layout

--- index.js
+++ index.js
@@ -3,38 +3,82 @@ let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
-
+var constraints = {
+    audio: false,
+    video: {
+        facingMode: 'user'
+    }
+}
+function takeshot() {
+  
+    let div =
+        document.getElementById('photoframe'); 
+     
+  
+    html2canvas(div,{ 
+        dpi: 144,
+        allowTaint: true
+      }).then(
+        
+        function (canvas) {
+            
+            return Canvas2Image.saveAsPNG(canvas);
+        })
+   
+}
 camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+   	let stream = await navigator.mediaDevices.getUserMedia(constraints);
 	video.srcObject = stream;
 });
-
+camera_button.click();
 click_button.addEventListener('click', function() {
    	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL('image/jpeg');
-
+   	let image_data_url = canvas.toDataURL('image/jpeg', 1.0); 
+    //takeshot();
    	// data url of the image
    //	console.log(image_data_url);
 });
 
-function takeshot() {
-  
-    let div =
-        document.getElementById('photoframe');
 
-    // Use the html2canvas
-    // function to take a screenshot
-    // and append it
-    // to the output div
-    html2canvas(div).then(
-        function (canvas) {
-            return Canvas2Image.saveAsPNG(canvas);
-        })
-    // html2canvas(div, {
-    //     onrendered: function(canvas) {
-    //         alert('fsafafa');
-    //         document.body.appendChild(canvas);
-    //       //return Canvas2Image.saveAsPNG(canvas);
-    //     }
-    // });
+
+const items = document.querySelectorAll('img');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
 }
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+
+$(document).ready(function(){
+    $('.frame').click(function(){
+        var imagePath = $(this).attr('src');
+        $('.card').css({"background-image": "url(" + imagePath + ")"});   
+       });
+});
